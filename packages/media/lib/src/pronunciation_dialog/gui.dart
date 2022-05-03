af6a4ffa378104c265a67df7a1e5677eef05eeb1
pavelpz <pzika@langmaster.cz>
2022-05-03 10:48:14 +0200

    fix

---
+++ packages/media/lib/src/pronunciation_dialog/gui.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+@swidget
+Widget pronuncDialog(String sourceUrl) {
+  return ProviderScope(child: );
+}
