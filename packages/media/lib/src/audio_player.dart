7895ea812715f50f0f9fc35873f7a5285f72f355
pavelpz <pzika@langmaster.cz>
2022-05-04 12:57:04 +0200

    fix

---
+++ packages/media/lib/src/audio_player.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+Future<AudioPlayerEx?> createPlayer(WidgetRef ref, {String? sourceUrl, void stateChanged(PlayerStateEx state)?}) {
+  if (sourceUrl == null) {
+    ref.audioPlayer = null;
+    return Future.value(null);
+  }
+  final res = AudioPlayerEx();
+  res.onPlayerStateChangedEx.forEach((stateEx) => stateChanged?.call(stateEx));
+  return res.setSourceUrl(sourceUrl).then((value) => ref.audioPlayer = res);
+}
+
+final pronuncUrlProvider = Provider.autoDispose<String?>((_) => throw UnimplementedError());
+final pronuncAudioPlayerProvider = StateNotifierProvider.autoDispose<AudioPlayerNotifier, AudioPlayerEx?>((_) => AudioPlayerNotifier(null));
+
+class AudioPlayerNotifier extends StateNotifier<AudioPlayerEx?> {
+  AudioPlayerNotifier(AudioPlayerEx? player) : super(player);
+  void setPlayer(AudioPlayerEx? player) => state = player;
+
+  @override
+  void dispose() {
+    state?.dispose();
+    super.dispose();
+  }
+}
+
+enum PlayerStateEx { other, playing, completed }
+
+class AudioPlayerEx extends AudioPlayer {
+  AudioPlayerEx() : super() {
+    _stateEx = StreamController<PlayerStateEx>();
+    onPlayerComplete.forEach((_) => _stateEx.add(PlayerStateEx.completed));
+    onPlayerStateChanged.forEach((s) => _stateEx.add(s == PlayerState.playing ? PlayerStateEx.playing : PlayerStateEx.other));
+  }
+
+  late StreamController<PlayerStateEx> _stateEx;
+  Stream<PlayerStateEx> get onPlayerStateChangedEx => _stateEx.stream;
+
+  @override
+  Future dispose() async {
+    await _stateEx.close();
+    await super.dispose();
+  }
+}
+
+extension WidgetRefPlayer on WidgetRef {
+  String? get sourceUrl => read(pronuncUrlProvider);
+  String? get watchSourceUrl => watch(pronuncUrlProvider);
+
+  AudioPlayerEx? get audioPlayer => read(pronuncAudioPlayerProvider);
+  set audioPlayer(AudioPlayerEx? player) => read(pronuncAudioPlayerProvider.notifier).setPlayer(player);
+  AudioPlayerEx? get watchAudioPlayer => watch(pronuncAudioPlayerProvider);
+}
---
+++ packages/media/lib/src/pronunciation_dialog.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'audio_player.dart';
+
+// flutter pub run build_runner watch --delete-conflicting-outputs
+part 'pronunciation_dialog.g.dart';
+
+enum PronuncState { none, playReady, playing, playRecGap, recReady, recording, recPlayGap, recPlayRecReady, recPlaying }
+
+final pronuncStateProvider = StateProvider.autoDispose<PronuncState>((_) => throw UnimplementedError());
+
+@swidget
+Widget pronuncDialog({required String? sourceUrl}) {
+  return ProviderScope(
+    overrides: [
+      pronuncUrlProvider.overrideWithValue(sourceUrl),
+      pronuncStateProvider.overrideWithValue(StateController<PronuncState>(sourceUrl == null ? PronuncState.none : PronuncState.playReady)),
+      pronuncAudioPlayerProvider,
+    ],
+    key: ValueKey(sourceUrl),
+    child: Row(
+      children: const [
+        PlayWrapper(),
+      ],
+    ),
+  );
+}
+
+@cwidget
+Widget playButton(WidgetRef ref) {
+  final player = ref.watchAudioPlayer;
+  return ElevatedButton(
+    onPressed: player == null
+        ? null
+        : () async {
+            await player.stop();
+            await player.resume();
+          },
+    child: Consumer(builder: (_, ref, ___) => Text(ref.watch(pronuncStateProvider) == PronuncState.playing ? 'Replay' : 'Play')),
+  );
+}
+
+@hcwidget
+Widget playProgress(WidgetRef ref) {
+  final player = ref.watchAudioPlayer;
+  final fDuration = useStream(player?.onDurationChanged, initialData: Duration());
+  final fPosition = useStream(player?.onPositionChanged, initialData: Duration());
+  return Text('${fPosition.hasData ? fPosition.data!.inMilliseconds : 0} / ${fDuration.hasData ? fDuration.data!.inMilliseconds : 0}');
+}
+
+@hcwidget
+Widget playWrapper(WidgetRef ref) {
+  final sourceUrl = ref.watchSourceUrl;
+  useEffect(() {
+    createPlayer(ref, sourceUrl: sourceUrl, stateChanged: (stateEx) {
+      if (stateEx == PlayerStateEx.other) return;
+      ref.state = stateEx == PlayerStateEx.playing ? PronuncState.playing : PronuncState.playRecGap;
+    });
+    return null;
+  }, [sourceUrl]);
+  return Column(children: [
+    PlayButton(),
+    PlayProgress(),
+    SizedBox(height: 20),
+    Consumer(builder: (_, ref, __) => Text(ref.watchState.toString())),
+  ]);
+}
+
+extension WidgetRefPronunc on WidgetRef {
+  PronuncState get state => read(pronuncStateProvider);
+  set state(PronuncState state) => read(pronuncStateProvider.notifier).state = state;
+  PronuncState get watchState => watch(pronuncStateProvider);
+}
+
+// ****************** TEST
+
+void main() {
+  runApp(const MyApp());
+}
+
+const mp3Files = [
+  'https://free-loops.com/data/mp3/c8/84/81a4f6cc7340ad558c25bba4f6c3.mp3',
+  'https://file-examples.com/storage/fed7f5feae62719de971a0c/2017/11/file_example_MP3_5MG.mp3',
+];
+
+@hcwidget
+Widget myApp() {
+  final state = useState(0);
+  final sourceUrl = state.value < 2 ? mp3Files[state.value] : null;
+  return MaterialApp(
+    title: 'PronuncDialog',
+    home: Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            PronuncDialog(sourceUrl: sourceUrl), //, key: ValueKey(sourceUrl ?? '')),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: () => state.value == 2 ? state.value = 0 : state.value++, child: Text('RUN ${state.value}')),
+            const SizedBox(height: 20),
+            PronuncDialog(
+                sourceUrl:
+                    'https://file-examples.com/storage/fed7f5feae62719de971a0c/2017/11/file_example_MP3_5MG.mp3'), //, key: ValueKey(sourceUrl ?? '')),
+          ],
+        ),
+      ),
+    ),
+  );
+}
